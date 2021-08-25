bd4a8b0207ded2bf75595934cd1301c9c3fd4c05
Felipe Pimenta <felimenta97@gmail.com>
2021-08-24 21:33:32 -0300

    Criação de arquivos para controller, service e business

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tcc_backend.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase {
+    }
+}
